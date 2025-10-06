242a452de4b2aa26aec51794783cc4bf19a81a65
Cihan Omur <mail@cihanomur.net>
2025-10-06 14:54:05 +0300

    1

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -17,11 +17,6 @@ class StudentController extends Controller
 {
     public function index()
     {
-        $user = auth()->user();
-
-        $user->roles; // atanmış roller
-
-        dd($user->getAllPermissions());
         $students = Student::with('guardians', 'emergencyContact', 'lessonClass')->where('registration_type', 2)->orderBy('full_name', 'asc')->get();
         $normalCount = Student::where('registration_type', 2)->count();
         $preCount = Student::where('registration_type', 1)->count();
