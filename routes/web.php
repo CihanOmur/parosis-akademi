e1465be3fb49e072d77b9b3d3d4406c94d5acb9e
Cihan Omur <mail@cihanomur.net>
2025-10-06 14:53:49 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -27,49 +27,49 @@ Route::middleware(['auth', SharedDatas::class])->prefix('panel')->group(function
     });
 
     Route::prefix('users')->name('users.')->group(function () {
-        Route::get('/', [UserController::class, 'index'])->name('index')->middleware('can:user');
-        Route::get('/create', [UserController::class, 'create'])->name('create')->middleware('can:user');
-        Route::post('/store', [UserController::class, 'store'])->name('store')->middleware('can:user');
-        Route::get('/{id}/edit', [UserController::class, 'edit'])->name('edit')->middleware('can:user');
-        Route::post('/{id}/update', [UserController::class, 'update'])->name('update')->middleware('can:user');
-        Route::delete('/{id}', [UserController::class, 'delete'])->name('delete')->middleware('can:user_delete');
+        Route::get('/', [UserController::class, 'index'])->name('index')->middleware('permission:user');
+        Route::get('/create', [UserController::class, 'create'])->name('create')->middleware('permission:user');
+        Route::post('/store', [UserController::class, 'store'])->name('store')->middleware('permission:user');
+        Route::get('/{id}/edit', [UserController::class, 'edit'])->name('edit')->middleware('permission:user');
+        Route::post('/{id}/update', [UserController::class, 'update'])->name('update')->middleware('permission:user');
+        Route::delete('/{id}', [UserController::class, 'delete'])->name('delete')->middleware('permission:user_delete');
     });
     Route::prefix('class')->name('class.')->group(function () {
-        Route::get('/', [LessonClassController::class, 'index'])->name('index')->middleware('can:class|class_delete');
-        Route::get('/create', [LessonClassController::class, 'create'])->name('create')->middleware('can:class');
-        Route::post('/store', [LessonClassController::class, 'store'])->name('store')->middleware('can:class');
-        Route::get('/{id}/edit', [LessonClassController::class, 'edit'])->name('edit')->middleware('can:class');
-        Route::post('/{id}/update', [LessonClassController::class, 'update'])->name('update')->middleware('can:class');
-        Route::delete('/{id}', [LessonClassController::class, 'delete'])->name('delete')->middleware('can:class_delete');
+        Route::get('/', [LessonClassController::class, 'index'])->name('index')->middleware('permission:class|class_delete');
+        Route::get('/create', [LessonClassController::class, 'create'])->name('create')->middleware('permission:class');
+        Route::post('/store', [LessonClassController::class, 'store'])->name('store')->middleware('permission:class');
+        Route::get('/{id}/edit', [LessonClassController::class, 'edit'])->name('edit')->middleware('permission:class');
+        Route::post('/{id}/update', [LessonClassController::class, 'update'])->name('update')->middleware('permission:class');
+        Route::delete('/{id}', [LessonClassController::class, 'delete'])->name('delete')->middleware('permission:class_delete');
     });
     Route::prefix('students')->name('students.')->group(function () {
-        Route::get('/', [StudentController::class, 'index'])->name('index')->middleware('role:SuperAdmin|Eğitmen|Muhasebe|Kordinatör');
-        Route::get('/create', [StudentController::class, 'create'])->name('create')->middleware('can:student');
-        Route::post('/store', [StudentController::class, 'store'])->name('store')->middleware('can:student');
+        Route::get('/', [StudentController::class, 'index'])->name('index')->middleware('permission:student|student_delete|accounting');
+        Route::get('/create', [StudentController::class, 'create'])->name('create')->middleware('permission:student');
+        Route::post('/store', [StudentController::class, 'store'])->name('store')->middleware('permission:student');
         Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('edit');
-        Route::get('/{id}/re-create', [StudentController::class, 'reCreate'])->name('reCreate')->middleware('can:student');
-        Route::post('/{id}/re-create', [StudentController::class, 'reCreateUpdate'])->name('reCreateUpdate')->middleware('can:student');
-        Route::post('/{id}/update', [StudentController::class, 'update'])->name('update')->middleware('can:student');
-        Route::post('/{id}/change-activity', [StudentController::class, 'changeActivity'])->name('changeActivity')->middleware('can:student');
+        Route::get('/{id}/re-create', [StudentController::class, 'reCreate'])->name('reCreate')->middleware('permission:student');
+        Route::post('/{id}/re-create', [StudentController::class, 'reCreateUpdate'])->name('reCreateUpdate')->middleware('permission:student');
+        Route::post('/{id}/update', [StudentController::class, 'update'])->name('update')->middleware('permission:student');
+        Route::post('/{id}/change-activity', [StudentController::class, 'changeActivity'])->name('changeActivity')->middleware('permission:student');
 
-        Route::get('/{id}/payment', [StudentController::class, 'payment'])->name('payment')->middleware('can:student|accounting');
-        Route::post('/{id}/payment', [StudentController::class, 'paymentUpdate'])->name('paymentUpdate')->middleware('can:student|accounting');
+        Route::get('/{id}/payment', [StudentController::class, 'payment'])->name('payment')->middleware('permission:student|accounting');
+        Route::post('/{id}/payment', [StudentController::class, 'paymentUpdate'])->name('paymentUpdate')->middleware('permission:student|accounting');
         Route::get('/{id}/payments', [StudentController::class, 'allPayments'])->name('allPayments')->middleware('permission:student|accounting');
 
-        Route::post('/downloadRegistrationForm', [StudentController::class, 'downloadRegistrationForm'])->name('downloadRegistrationForm')->middleware('can:student|student_delete|accounting');
-        Route::post('/downloadContract', [StudentController::class, 'downloadContract'])->name('downloadContract')->middleware('can:student|accounting|student_delete');
-        Route::post('/downloadPayment', [StudentController::class, 'downloadPayment'])->name('downloadPayment')->middleware('can:student|accounting|student_delete');
+        Route::post('/downloadRegistrationForm', [StudentController::class, 'downloadRegistrationForm'])->name('downloadRegistrationForm')->middleware('permission:student|student_delete|accounting');
+        Route::post('/downloadContract', [StudentController::class, 'downloadContract'])->name('downloadContract')->middleware('permission:student|accounting|student_delete');
+        Route::post('/downloadPayment', [StudentController::class, 'downloadPayment'])->name('downloadPayment')->middleware('permission:student|accounting|student_delete');
 
-        Route::get('/create-pre-registiration', [StudentController::class, 'createPreRegistiration'])->name('pre.createPreRegistiration')->middleware('can:student');
-        Route::post('/store-pre-registiration', [StudentController::class, 'storePreRegistiration'])->name('pre.storePreRegistiration')->middleware('can:student');
-        Route::get('/{id}/edit-pre-registiration', [StudentController::class, 'editPreRegistiration'])->name('pre.editPreRegistiration')->middleware('can:student');
-        Route::post('/{id}/update-pre-registiration', [StudentController::class, 'updatePreRegistiration'])->name('pre.updatePreRegistiration')->middleware('can:student');
+        Route::get('/create-pre-registiration', [StudentController::class, 'createPreRegistiration'])->name('pre.createPreRegistiration')->middleware('permission:student');
+        Route::post('/store-pre-registiration', [StudentController::class, 'storePreRegistiration'])->name('pre.storePreRegistiration')->middleware('permission:student');
+        Route::get('/{id}/edit-pre-registiration', [StudentController::class, 'editPreRegistiration'])->name('pre.editPreRegistiration')->middleware('permission:student');
+        Route::post('/{id}/update-pre-registiration', [StudentController::class, 'updatePreRegistiration'])->name('pre.updatePreRegistiration')->middleware('permission:student');
 
-        Route::get('/{id}/pre-to-normal', [StudentController::class, 'preToNormal'])->name('pre-to-normal')->middleware('can:student');
-        Route::post('/{id}/pre-to-normal', [StudentController::class, 'preToNormalPost'])->name('pre-to-normal.post')->middleware('can:student');
+        Route::get('/{id}/pre-to-normal', [StudentController::class, 'preToNormal'])->name('pre-to-normal')->middleware('permission:student');
+        Route::post('/{id}/pre-to-normal', [StudentController::class, 'preToNormalPost'])->name('pre-to-normal.post')->middleware('permission:student');
 
-        Route::get('/pre/students', [StudentController::class, 'preStudents'])->name('pre.students')->middleware('can:student');
-        Route::delete('/{id}', [StudentController::class, 'delete'])->name('delete')->middleware('can:student_delete');
+        Route::get('/pre/students', [StudentController::class, 'preStudents'])->name('pre.students')->middleware('permission:student');
+        Route::delete('/{id}', [StudentController::class, 'delete'])->name('delete')->middleware('permission:student_delete');
     });
 });
 Route::get('/', function () {
