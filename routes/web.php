b1bc98393bdaf1f3b52c1ba773575e58d252e62e
Cihan Omur <mail@cihanomur.net>
2025-10-06 14:42:25 +0300

    1

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::middleware(['auth', SharedDatas::class])->prefix('panel')->group(function
         Route::delete('/{id}', [LessonClassController::class, 'delete'])->name('delete')->middleware('can:class_delete');
     });
     Route::prefix('students')->name('students.')->group(function () {
-        Route::get('/', [StudentController::class, 'index'])->name('index')->middleware('permission:student,student_delete,');
+        Route::get('/', [StudentController::class, 'index'])->name('index')->middleware('permission:student|student_delete,');
         Route::get('/create', [StudentController::class, 'create'])->name('create')->middleware('can:student');
         Route::post('/store', [StudentController::class, 'store'])->name('store')->middleware('can:student');
         Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('edit');
