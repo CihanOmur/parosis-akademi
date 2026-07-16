e48601ad2b1444a7888ac7d305f1b84a5dbe9bfe
Your Name <you@example.com>
2026-07-16 20:33:04 +0000

    fix(product,css): sepete ekle butonu kartlarla BIREBIR ayni (is-icon original)
    Uc iterasyon boyunca CSS bozmustum, simdi orijinal degerlere donduruldu:
    - .is-icon padding-right: 70px, hover padding-left: 70px
    - .btn-icon 44px (2.75rem)
    - Vertical align yok (orijinal davranis)
    
    Product detail butonu artik kart butonlariyla BIREBIR ayni HTML+CSS.
    flex-1/min-w gibi ekstra class'lar kaldirildi, content'e gore genislik.

--- resources/views/front/pages/product-details.blade.php
+++ resources/views/front/pages/product-details.blade.php
@@ -263,15 +263,18 @@
                                                 </button>
                                             </div>
 
-                                            <!-- Add to Cart Button -->
+                                            <!-- Add to Cart Button (kartlardaki gibi is-icon) -->
                                             <button type="submit"
                                                     id="add-to-cart-btn"
                                                     @if($product->variants && $product->variants->count()) disabled @endif
-                                                    class="inline-flex items-center justify-center gap-2.5 whitespace-nowrap rounded-full bg-colorPurpleBlue px-8 py-4 text-base font-medium text-white transition hover:brightness-110 hover:shadow-lg flex-1 sm:flex-none sm:min-w-[220px] disabled:cursor-not-allowed disabled:opacity-50">
-                                                <svg class="h-5 w-5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/>
-                                                </svg>
+                                                    class="btn btn-primary is-icon group disabled:cursor-not-allowed disabled:opacity-50">
                                                 <span @if($fs('detail_add_to_cart')) style="{{ $fs('detail_add_to_cart') }}" @endif>{{ $shopInfo->detail_add_to_cart ?? 'Sepete Ekle' }}</span>
+                                                <span class="btn-icon bg-white group-hover:right-0 group-hover:translate-x-full">
+                                                    <img src="{{ asset('assets-front/img/icons/icon-purple-shopping-cart-line.svg') }}" alt="cart" width="20" height="20" />
+                                                </span>
+                                                <span class="btn-icon bg-white group-hover:left-[5px] group-hover:translate-x-0">
+                                                    <img src="{{ asset('assets-front/img/icons/icon-purple-shopping-cart-line.svg') }}" alt="cart" width="20" height="20" />
+                                                </span>
                                             </button>
                                         </div>
                                     </form>
