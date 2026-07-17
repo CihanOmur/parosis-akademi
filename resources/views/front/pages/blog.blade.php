92dd78069321372436ea15ede78b5846146a6a8c
Your Name <you@example.com>
2026-07-17 18:10:41 +0000

    ui(blog): pb-44 -> pb-16 lg:pb-20 — footer ile aradaki bosluk azaltildi

--- resources/views/front/pages/blog.blade.php
+++ resources/views/front/pages/blog.blade.php
@@ -62,7 +62,7 @@
 
             <!--...::: Blog Section Start :::... -->
             <div class="section-blog">
-                <div class="bg-white pb-44">
+                <div class="bg-white pb-16 lg:pb-20">
                     <!-- Section Space -->
                     <div class="section-space">
                         <!-- Section Container -->
